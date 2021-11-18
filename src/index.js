32e9b2749bda70de348d9f7773afd7242a532dbf
Arun Yokesh <ayokesh.cse@gmail.com>
2021-11-18 23:30:48 +0530

    demo published

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1 className="header">React Instagram Streamer</h1>
+    <h1 className="header">React Instagram Streamer Demo</h1>
     <div className="container">
         <InstagramStreamer accessToken="IGQVJXcDMybmg5eTBiS2dpdjFyYkRMRENlREg0UzVTdjNaU01OSW9BX1Rzby10a3BST3RTbExBemtGcEhjWEVnS0xDQUQwa3NhdF9hbkdncXdVMXBZAMVFSWkRXUVNkYy1HdG9zMFE3VWVVNlJNMFlhZAwZDZD" />
     </div>
