5d17869e4d499d7fb97af3099b4ded42a42bb48b
Arun Yokesh <ayokesh.cse@gmail.com>
2021-11-24 19:09:48 +0530

    structure changes

---
+++ src/demo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { InstagramStreamer } from 'react-instagram-streamer';
+import './index.css';
+
+const Demo = () => {
+
+	return (
+		<>
+			<h1 className="header">React Instagram Streamer Demo</h1>
+			<div className="container">
+				<InstagramStreamer accessToken='IGQVJXcDMybmg5eTBiS2dpdjFyYkRMRENlREg0UzVTdjNaU01OSW9BX1Rzby10a3BST3RTbExBemtGcEhjWEVnS0xDQUQwa3NhdF9hbkdncXdVMXBZAMVFSWkRXUVNkYy1HdG9zMFE3VWVVNlJNMFlhZAwZDZD' />
+			</div>
+		</>
+	);
+
+}
+
+export default Demo;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {InstagramStreamer} from 'react-instagram-streamer';
-import './index.css';
+import Demo from './demo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1 className="header">React Instagram Streamer Demo</h1>
-    <div className="container">
-        <InstagramStreamer accessToken="IGQVJXcDMybmg5eTBiS2dpdjFyYkRMRENlREg0UzVTdjNaU01OSW9BX1Rzby10a3BST3RTbExBemtGcEhjWEVnS0xDQUQwa3NhdF9hbkdncXdVMXBZAMVFSWkRXUVNkYy1HdG9zMFE3VWVVNlJNMFlhZAwZDZD" />
-    </div>
+    <Demo />
   </React.StrictMode>,
   document.getElementById('root')
 );
