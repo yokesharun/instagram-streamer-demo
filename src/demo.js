18d6cbbb1cca91e2efa835956918f33622ce8d95
Arun Yokesh <ayokesh.cse@gmail.com>
2021-11-29 20:48:27 +0530

    updated to new package

--- src/demo.js
+++ src/demo.js
@@ -4,9 +4,11 @@ import './index.css';
 
 const Demo = () => {
 
+
 	return (
 		<>
 			<h1 className="header">React Instagram Streamer Demo</h1>
+			<p className="info">Wait for 10 seconds to see the magic :)</p>
 			<div className="container">
 				<InstagramStreamer accessToken='IGQVJXcDMybmg5eTBiS2dpdjFyYkRMRENlREg0UzVTdjNaU01OSW9BX1Rzby10a3BST3RTbExBemtGcEhjWEVnS0xDQUQwa3NhdF9hbkdncXdVMXBZAMVFSWkRXUVNkYy1HdG9zMFE3VWVVNlJNMFlhZAwZDZD' />
 			</div>
